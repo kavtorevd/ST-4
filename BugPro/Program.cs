9c5f7fe206503b419f39f690ac1f40588e34ae3a
Dmitry Kavtorev <133030437+kavtorevd@users.noreply.github.com>
2025-05-31 15:08:46 +0300

    Fix code

--- BugPro/Program.cs
+++ BugPro/Program.cs
@@ -1,85 +1,64 @@
-using Stateless;
-using System;
+﻿using Stateless;
+
 public class Bug
 {
-    public enum State { Open, Assigned, Defered, Closed, Reopened, Verified, Rejected }
-    private enum Trigger { Assign, Defer, Close, Reopen, Verify, Reject }
+    public enum State { Open, Assigned, Defered, Closed, CreatedFixes, DeclinedFixes, AcceptedFixes }
+    private enum Trigger { Assign, Defer, Close, CreateFix, DeclineFix, AcceptFix }
     private StateMachine<State, Trigger> sm;
 
     public Bug(State state)
     {
         sm = new StateMachine<State, Trigger>(state);
-        
         sm.Configure(State.Open)
-            .Permit(Trigger.Assign, State.Assigned)
-            .Permit(Trigger.Close, State.Closed)
-            .Ignore(Trigger.Defer);
-        
+              .Permit(Trigger.Assign, State.Assigned);
         sm.Configure(State.Assigned)
-            .Permit(Trigger.Close, State.Closed)
-            .Permit(Trigger.Defer, State.Defered)
-            .Permit(Trigger.Reject, State.Rejected)
-            .Ignore(Trigger.Assign);
-        
+              .Permit(Trigger.Close, State.Closed)
+              .Permit(Trigger.Defer, State.Defered)
+              .Ignore(Trigger.Assign);
         sm.Configure(State.Closed)
-            .Permit(Trigger.Reopen, State.Reopened)
-            .Permit(Trigger.Verify, State.Verified)
-            .Ignore(Trigger.Close);
-        
+              .Permit(Trigger.Assign, State.Assigned);
         sm.Configure(State.Defered)
-            .Permit(Trigger.Assign, State.Assigned)
-            .Permit(Trigger.Close, State.Closed);
-        
-        sm.Configure(State.Reopened)
-            .Permit(Trigger.Assign, State.Assigned)
-            .Permit(Trigger.Close, State.Closed);
-        
-        sm.Configure(State.Verified)
-            .Permit(Trigger.Reopen, State.Reopened)
-            .Permit(Trigger.Close, State.Closed);
-        
-        sm.Configure(State.Rejected)
-            .Permit(Trigger.Assign, State.Assigned)
+              .Permit(Trigger.Assign, State.Assigned);
+        sm.Configure(State.CreatedFixes)
+            .Permit(Trigger.AcceptFix, State.AcceptedFixes)
+            .Permit(Trigger.DeclineFix, State.DeclinedFixes)
+            .Ignore(Trigger.CreateFix);
+        sm.Configure(State.DeclinedFixes)
+            .Permit(Trigger.CreateFix, State.CreatedFixes);
+        sm.Configure(State.AcceptedFixes)
             .Permit(Trigger.Close, State.Closed);
     }
-
     public void Close()
     {
         sm.Fire(Trigger.Close);
         Console.WriteLine("Close");
     }
-
     public void Assign()
     {
         sm.Fire(Trigger.Assign);
         Console.WriteLine("Assign");
     }
-
     public void Defer()
     {
         sm.Fire(Trigger.Defer);
         Console.WriteLine("Defer");
     }
-
-    public void Reopen()
+    public void CreateFix()
     {
-        sm.Fire(Trigger.Reopen);
-        Console.WriteLine("Reopen");
+        sm.Fire(Trigger.CreateFix);
+        Console.WriteLine("Create Fix");
     }
-
-    public void Verify()
+    public void DeclineFix()
     {
-        sm.Fire(Trigger.Verify);
-        Console.WriteLine("Verify");
+        sm.Fire(Trigger.DeclineFix);
+        Console.WriteLine("Decline Fix");
     }
-
-    public void Reject()
+    public void AcceptFix()
     {
-        sm.Fire(Trigger.Reject);
-        Console.WriteLine("Reject");
+        sm.Fire(Trigger.AcceptFix);
+        Console.WriteLine("Accept Fix");
     }
-
-    public State GetState()
+    public State getState()
     {
         return sm.State;
     }
@@ -92,14 +71,9 @@ public class Program
         var bug = new Bug(Bug.State.Open);
         bug.Assign();
         bug.Close();
-        bug.Reopen();
         bug.Assign();
         bug.Defer();
         bug.Assign();
-        bug.Reject();
-        bug.Assign();
-        bug.Close();
-        bug.Verify();
-        Console.WriteLine(bug.GetState());
+        Console.WriteLine(bug.getState());
     }
 }
